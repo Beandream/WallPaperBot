3b5b27376042f62b77d41e9dc94bbd76fa5edd2e
Beandream <beandreamed@gmail.com>
2018-05-07 09:41:20 -0400

    Revised Submitting Format
    Errors for wrong channels or no image etc.

--- index.js
+++ index.js
@@ -22,57 +22,43 @@ client.on('message', msg => {
       return;
     };
     if (submit > -1){
-      var submitError = -1;
-      let attachArray = Array.from(msg.attachments.values()); 
-      attachArray.forEach(att => {
-        if(att.height > 0){
-          var textChannel = msg.guild.channels.find("name", "a");
-          textChannel.fetchMessages().then(messages =>{
-            let messageArray = Array.from(messages);
-            messageArray.forEach((Arraycontent) =>{
-              var msgSplit = Arraycontent[Arraycontent.length -1 ];
-              if (msgSplit.content.search(msg.author) > -1){
-                submitError = 1;
-              };
-            })
-            if (submitError < 1){
-              textChannel.send(`<@${msg.author.id}>`);
-              textChannel.send(att.url);
-              msg.channel.send('**Image Succesfully Submitted**');
-              submitError = -1;
-              return;
-            }
-            msg.channel.send('**Error**- Image Already Submitted');
-          });
+      if (msg.channel.id == '442805569485537290'){
+        var submitError = -1;
+        let attachArray = Array.from(msg.attachments.values()); 
+        attachArray.forEach(att => {
+          if(att.height > 0){
+            var textChannel = msg.guild.channels.find("name", "a");
+            textChannel.fetchMessages().then(messages =>{
+              let messageArray = Array.from(messages);
+              messageArray.forEach((Arraycontent) =>{
+                var msgSplit = Arraycontent[Arraycontent.length -1 ];
+                if (msgSplit.content.search(msg.author) > -1){
+                  submitError = 1;
+                };
+              })
+              if (submitError < 1){
+                textChannel.send(`<@${msg.author.id}>`);
+                textChannel.send(att.url);
+                msg.channel.send('**Image Succesfully Submitted**');
+                submitError = -1;
+                return;
+              }
+              msg.channel.send('**Error**-You Already Submitted An Image');
+            });
+          }; 
+        });
+        if (!attachArray.length){
+          msg.channel.send('Please Submit An Image');  
         }; 
-      });
-      if (!attachArray.length){
-        msg.channel.send('Please Submit An Image');  
-      }; 
+      };
+      if (msg.channel.id !== '442805569485537290'){
+        msg.delete(0, 1);
+        var txtChannel = msg.guild.channels.find("name", "generalchat");
+        msg.channel.send(`Please take this to ${txtChannel}, ${msg.author.username}!`).then (Mymsg => {
+          Mymsg.delete(5000);
+        });
+      };
     };
-    // if(search > 0){
-    //   var textChannel = msg.guild.channels.find("name", "generalchat"); 
-    //   textChannel.fetchMessages().then(messages => {
-    //     var filteredMsgCollection = messages.filter(msg =>{
-    //       if (msg.isMemberMentioned(client.user)){
-    //         let firstAttchmentArray = Array.from(msg.attachments.values());  
-    //         return msg.content.search("hi") !== -1 && firstAttchmentArray.length > 0
-    //       };
-    //     })
-    //     var messageArray = filteredMsgCollection.array();
-    //     messageArray.forEach(message => {
-    //       let attachArray = Array.from(message.attachments.values()); 
-    //       attachArray.forEach(att => {
-    //         if(att.height > 0){
-    //           msg.channel.send(`found image`);
-    //           msg.channel.send(att.url);
-    //           msg.channel.send(msg.author.username);
-    //         };
-    //       })
-    //     })
-    //     msg.channel.send(`found ${messageArray.length} messages with the word hi`)
-    //   }).catch(console.error);
-    // };
   };
 });
  
